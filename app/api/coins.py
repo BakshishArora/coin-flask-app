69a2652c8c4b1fdbda416c26e8cd1baabaf84d7c
Bakshish Arora <bakshish2231@gmail.com>
2024-12-20 01:45:28 +0530

    adds coins namespace

---
+++ app/api/coins.py
@@ -0,0 +1,17 @@
+from flask_restx import Namespace, Resource
+from controllers.coins_controller import CoinsController
+
+
+api = Namespace('coins', description='Coins related operations')
+
+
+@api.route('/api/v1.0/coins/list', methods=['GET'])
+class Coins(Resource):
+    def get(self):
+        return CoinsController.get_coins_list()
+
+
+@api.route('/api/v1.0/coins/categories/list', methods=['GET'])
+class CoinCategories(Resource):
+    def get(self):
+        return CoinsController.get_coins_categories_list()
