0aa6a441f388a738a007a03152ad3fc9248264de
Bakshish Arora <bakshish2231@gmail.com>
2024-12-20 01:41:21 +0530

    adds pagination function

---
+++ app/services/utils.py
@@ -0,0 +1,20 @@
+from flask_restful import Resource, reqparse
+import requests
+from config import Config
+
+
+class Utilities():
+    @classmethod
+    def pagination(cls, page_num, per_page):
+        start,end = 1, Config.DEFAULT_PAGE_SIZE
+        try:
+            if page_num < 1:
+                raise Exception("Page Number cannot be lesser than 1.")
+            if per_page == 0:
+                raise Exception("No data is chosen to be returned.")
+            start = (page_num - 1) * per_page
+            end = start + per_page
+            return start, end 
+        except Exception as e:
+            raise e
+
